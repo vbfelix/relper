ef5e7ff05ba303121d72a67f4021f9919be9f746
Vinícius Félix <felix_prot@hotmail.com>
2020-05-09 19:15:58 -0300

    add auc

---
+++ R/auc.r
@@ -0,0 +1,22 @@
+#' Area under the curve (AUC)
+#'
+#' @description Calculates AUC
+#'
+#' @param x numeric vector.
+#' @param y numeric vector.
+#'
+#' @return auc
+#' @export
+#'
+#' @examples
+#' x <- seq(-3,3,l = 100)
+#' y <- dnorm(x)
+#' plot(x,y)
+#' auc(x,y)
+auc  <- function(x,y) {
+    tryCatch({
+    integrate(approxfun(x,y), range(x)[1], range(x)[2])$value
+  },
+  error = function(e) return(NA_real_)
+  )
+}
