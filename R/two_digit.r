8d7555d8ba5182570e37581aac8f2383b0f41af8
Vinícius Félix <felix_prot@hotmail.com>
2020-05-11 17:57:21 -0300

    add func two_digit

---
+++ R/two_digit.r
@@ -0,0 +1,21 @@
+#' Two Digits
+#'
+#' @description Format 1 as "01"
+#'
+#' @param x numeric vector
+#' @return character vector
+#' @export
+#'
+#' @examples
+#'
+#' x <- c(1,4,10,12)
+#'
+#' two_digit(x)
+#'
+
+two_digit <- function(x){
+  ifelse(nchar(x)==1,
+         paste0("0",x),
+         x)
+}
+
