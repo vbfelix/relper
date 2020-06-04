da7ae0ecf1008e8e7c96f29350a3244da996219d
Vinícius Félix <felix_prot@hotmail.com>
2020-06-04 03:40:29 -0300

    update func tbl_plot - add 2 warnings

--- R/tbl_plot.r
+++ R/tbl_plot.r
@@ -18,10 +18,18 @@
 #'
 #'
 
-tbl_plot <- function(tbl,header_col = "grey75",base_size = 20){
+tbl_plot <- function(tbl,header_col = "grey75",base_size = 15){
 
   if( !(class(tbl)[1] %in% c("tbl_df","tbl","data.frame","tabyl","grouped_df"))){
     stop("tbl must be a data.frame/tibble")
+  }
+
+  if(nrow(tbl) > 25){
+    warning("numbers of rows is to long")
+  }
+
+  if(ncol(tbl) > 8){
+    warning("numbers of columns is to long")
   }
 
   plot(1)
@@ -30,11 +38,12 @@ tbl_plot <- function(tbl,header_col = "grey75",base_size = 20){
 
   n <- nrow(tbl)
 
-  tbl_theme <- gridExtra::ttheme_default(
-    base_size = base_size,
-    colhead = list(bg_params = list(fill = header_col, col=NA)),
-    core = list(bg_params = list(fill = NA))
-  )
+  tbl_theme <-
+    gridExtra::ttheme_default(
+      base_size = base_size,
+      colhead = list(bg_params = list(fill = header_col, col=NA)),
+      core = list(bg_params = list(fill = NA))
+    )
 
   out <- gridExtra::tableGrob(tbl,rows = NULL, theme = tbl_theme)
 
