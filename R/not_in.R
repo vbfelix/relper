0952f7ae23b727f224dd4fbe96532ddfbbea6803
Vinícius Félix <felix_prot@hotmail.com>
2020-08-14 17:00:16 -0300

    add func not_in; not_na

---
+++ R/not_in.R
@@ -0,0 +1,20 @@
+#' Opposite of %in%
+#'
+#' @description Check if a x are not in y
+#'
+#' @param x single value
+#' @param y vector
+#'
+#' @return boolean
+#' @export
+#'
+#' @examples
+#'
+#' not_in(a, letters)
+#'
+
+not_in <- function(x,y){
+
+  !(x %in% y)
+
+}
---
+++ R/not_na.R
@@ -0,0 +1,21 @@
+#' Opposite of is.na
+#'
+#' @description Check if the value is not a NA
+#'
+#' @param x single value or vector
+#'
+#' @return boolean
+#' @export
+#'
+#' @examples
+#'
+#' not_na(2)
+#'
+#' not_na(NA)
+#'
+
+not_na <- function(x){
+
+!is.na(x)
+
+}
