77ed2539c777f061161e2fad797bc4dd4a397700
vbfelix <felix_prot@hotmail.com>
2021-03-20 19:42:35 -0300

    add func scale_auto add func tbl_num_format

---
+++ R/tbl_num_format.R
@@ -0,0 +1,22 @@
+#' Format data.frame
+#'
+#' @description Apply num_format to all numeric columns in a data.frame
+#'
+#' @param df data.frame
+#' @param digits number of digits
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' mtcars %>%
+#'   count(vs,am) %>%
+#'   tbl_num_format(digits = 5)
+
+tbl_num_format <- function(df,digits){
+
+  df %>%
+    dplyr::mutate(dplyr::across(where(is.numeric),~relper::num_format(.,digits = digits)))
+
+}
