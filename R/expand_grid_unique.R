e4031b1adde4cb549c09dd919bbab687368c1bb6
vbfelix <felix_prot@hotmail.com>
2021-06-02 23:35:00 -0300

    add func expand_grid_unique

---
+++ R/expand_grid_unique.R
@@ -0,0 +1,26 @@
+#' expand_grid without repetition
+#'
+#' @description
+#'
+#' @param x
+#' @param y
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#'
+
+expand_grid_unique <- function(x, y, include.equals=FALSE)
+{
+  x <- unique(x)
+  y <- unique(y)
+  g <- function(i)
+  {
+    z <- setdiff(y, x[seq_len(i-include.equals)])
+    if(length(z)) cbind(x[i], z, deparse.level=0)
+  }
+  do.call(rbind, lapply(seq_along(x), g)) %>%
+    as_tibble()
+}
