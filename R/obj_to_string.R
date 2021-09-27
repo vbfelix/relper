7ef9028630671fc83310f9c0ee3e7a15676c9a84
vbfelix <felix_prot@hotmail.com>
2021-09-26 23:41:24 -0300

    updated description

---
+++ R/obj_to_string.R
@@ -0,0 +1,24 @@
+#' Object to string
+#'
+#' @description Get the name of the object and return as a string
+#'
+#' @param x
+#'
+#' @return vector
+#' @export
+#'
+#' @examples
+#'
+#'
+#' x <- rnorm(100)
+#'
+#' obj_to_string(x)
+#'
+#'
+
+obj_to_string <-
+  function(x){
+
+    deparse(substitute(x))
+
+  }
