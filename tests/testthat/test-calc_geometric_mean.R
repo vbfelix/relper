2325ea3eb562b50c7a104b055f456acb594cfdb6
vbfelix <felix_prot@hotmail.com>
2022-12-08 00:10:49 -0300

    add tests for calc_geometric_mean

--- tests/testthat/test-calc_geometric_mean.R
+++ tests/testthat/test-calc_geometric_mean.R
@@ -1,3 +1,21 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("results", {
+
+  expect_equal(
+    object = calc_geometric_mean(1:10),
+    expected = 4.52872868811677
+  )
+
+})
+
+test_that("warning", {
+
+  expect_warning(calc_geometric_mean(0))
+
+})
+
+test_that("error", {
+
+  expect_error(calc_geometric_mean("a"))
+
 })
