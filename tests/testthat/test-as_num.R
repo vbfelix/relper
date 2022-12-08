3ebd74ce8b40cb7bc8a73dd273033447eac522c5
vbfelix <felix_prot@hotmail.com>
2022-12-07 21:29:34 -0300

    tests for as_num

--- tests/testthat/test-as_num.R
+++ tests/testthat/test-as_num.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("results", {
+  expect_equal(as_num(x = "1.000"), 1000)
+  expect_equal(as_num(x = "1.000,20"), 1000.2)
+  expect_equal(as_num(x = "0,20"), .2)
+
+})
+
+test_that("warning", {
+  expect_warning(as_num(x = "a"))
+
 })
