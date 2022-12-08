ba0d277c201f15a52ab9bb759ca54a5d80ad38b3
vbfelix <felix_prot@hotmail.com>
2022-12-08 00:13:46 -0300

    add tests for calc_mean

--- tests/testthat/test-calc_mean.R
+++ tests/testthat/test-calc_mean.R
@@ -1,3 +1,23 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("results", {
+
+  expect_equal(
+    object = calc_mean(1:3),
+    expected = structure(list(arithmetic = 2, geometric = 1.81712059283214,
+                              harmonic = 1.63636363636364),
+                         row.names = c(NA, -1L),
+                         class = c("tbl_df","tbl", "data.frame"))
+  )
+
+})
+
+test_that("warning", {
+
+
+})
+
+test_that("error", {
+
+  expect_error(calc_mean("a"))
+
 })
