d86a3ed187dfd0f57dd01fa29072a6f7407e65a5
vbfelix <felix_prot@hotmail.com>
2022-12-08 00:19:50 -0300

    add tests for cut_by_quantile

--- tests/testthat/test-cut_by_quantile.R
+++ tests/testthat/test-cut_by_quantile.R
@@ -1,3 +1,23 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("results", {
+
+  expect_equal(
+    object = cut_by_quantile(x = 1:3,q = 0:1),
+    expected = structure(c(1L, 1L, 1L), .Label = "[1,3]", class = "factor")
+  )
+
+})
+
+test_that("warning", {
+
+
+})
+
+test_that("error", {
+
+  expect_error(cut_by_quantile(x = "a",q = 1))
+  expect_error(cut_by_quantile(x = 1,q = "a"))
+  expect_error(cut_by_quantile(x = 1,q = 1))
+  expect_error(cut_by_quantile(x = 1,q = seq(0,1,.1)))
+  expect_error(cut_by_quantile(x = 1:10,q = 1:10))
+
 })
