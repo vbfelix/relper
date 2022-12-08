a97e0988b90593e2cc96504e50dc04633f2e9e1d
vbfelix <felix_prot@hotmail.com>
2022-12-08 00:12:08 -0300

    add tests for calc_harmonic_mean

--- tests/testthat/test-calc_harmonic_mean.R
+++ tests/testthat/test-calc_harmonic_mean.R
@@ -1,3 +1,21 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("results", {
+
+  expect_equal(
+    object = calc_harmonic_mean(1:10),
+    expected = 3.41417152147406
+  )
+
+})
+
+test_that("warning", {
+
+  expect_warning(calc_harmonic_mean(0))
+
+})
+
+test_that("error", {
+
+  expect_error(calc_harmonic_mean("a"))
+
 })
