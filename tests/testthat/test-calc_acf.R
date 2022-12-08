6c8c3d06cdb123c91a2940f1f503dce161eb3cd0
vbfelix <felix_prot@hotmail.com>
2022-12-07 21:36:22 -0300

    tests for calc_auc

--- tests/testthat/test-calc_acf.R
+++ tests/testthat/test-calc_acf.R
@@ -1,6 +1,6 @@
 test_that("error", {
-  expect_error(as_perc(x = "a"))
-  expect_error(as_perc(y = "a"))
-  expect_error(as_perc(x = 1,y = c(1,2)))
-  expect_error(as_perc(x = 1,y = 1))
+  expect_error(calc_acf(x = "a"))
+  expect_error(calc_acf(y = "a"))
+  expect_error(calc_acf(x = 1,y = c(1,2)))
+  expect_error(calc_acf(x = 1,y = 1))
 })
--- tests/testthat/test-calc_auc.R
+++ tests/testthat/test-calc_auc.R
@@ -1,3 +1,16 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("results", {
+  expect_equal(calc_auc(x = 1:5,y = rep(2,5)), 8)
+  expect_equal(calc_auc(x = 1:5,y = rep(2,5),limits = c(1,4)), 6)
+})
+
+
+test_that("error", {
+  expect_error(calc_auc(x = "a"))
+  expect_error(calc_auc(y = "a"))
+  expect_error(calc_auc(limits = "a"))
+  expect_error(calc_auc(limits = 1))
+  expect_error(calc_auc(limits = 1:3))
+
+
 })
