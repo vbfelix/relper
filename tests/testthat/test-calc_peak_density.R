e9632245b0fb411128f834e7cbc5e54c320dd186
vbfelix <felix_prot@hotmail.com>
2022-12-08 00:16:49 -0300

    add tests for calc_peak_density

--- tests/testthat/test-calc_peak_density.R
+++ tests/testthat/test-calc_peak_density.R
@@ -1,3 +1,21 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("results", {
+
+  expect_equal(
+    object = calc_peak_density(1:2),
+    expected = 1.00685002093889
+  )
+
+})
+
+test_that("warning", {
+
+
+})
+
+test_that("error", {
+
+  expect_error(calc_peak_density("a"))
+  expect_error(calc_peak_density(1))
+
+
 })
