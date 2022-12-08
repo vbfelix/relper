502f85b8533676e07d90933b47ed8177bced806b
vbfelix <felix_prot@hotmail.com>
2022-12-08 00:15:08 -0300

    add tests for calc_mode

--- tests/testthat/test-calc_mode.R
+++ tests/testthat/test-calc_mode.R
@@ -1,3 +1,20 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("results", {
+
+  expect_equal(
+    object = calc_mode(c("a","a","a","b")),
+    expected = "a"
+  )
+
 })
+
+test_that("warning", {
+
+
+})
+
+test_that("error", {
+
+
+})
+
