607334169c01ebe65fe253080cef8b00f5a59088
vbfelix <felix_prot@hotmail.com>
2022-12-11 22:42:33 -0300

    add tests for format_scale

--- tests/testthat/test-format_scale.R
+++ tests/testthat/test-format_scale.R
@@ -1,3 +1,36 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("results", {
+
+  expect_equal(
+    object = format_scale(1:2),
+    expected = c(0, 1)
+  )
+
+  expect_equal(
+    object = format_scale(1:2, new_min = 1, new_max = 5),
+    expected = c(1,5)
+  )
+
+})
+
+test_that("warning", {
+
+
+})
+
+test_that("error", {
+
+  expect_error(format_scale(x = "a"))
+
+  expect_error(format_scale(x = 1))
+
+  expect_error(format_scale(x = 1:2, new_min = 5, new_max = 1))
+
+  expect_error(format_scale(x = 1:2, new_min = "a", new_max = 1))
+
+  expect_error(format_scale(x = 1:2, new_min = 1, new_max = "a"))
+
+  expect_error(format_scale(x = 1:2, new_min = 5:10, new_max = 100))
+
+  expect_error(format_scale(x = 1:2, new_min = 5, new_max = 10:100))
+
 })
