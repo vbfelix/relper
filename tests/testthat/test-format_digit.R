50e2d33d237670cec50e959c8bc8ed8b0e847e03
vbfelix <felix_prot@hotmail.com>
2022-12-11 22:23:47 -0300

    add tests for format_digit

--- tests/testthat/test-format_digit.R
+++ tests/testthat/test-format_digit.R
@@ -1,3 +1,24 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("results", {
+
+  expect_equal(
+    object = format_digit(2),
+    expected = "02"
+  )
+
+  expect_equal(
+    object = format_digit(2,digits = 3),
+    expected = "002"
+  )
+
+})
+
+test_that("warning", {
+
+
+})
+
+test_that("error", {
+
+  expect_error(format_digit(2,digits = "a"))
+
 })
