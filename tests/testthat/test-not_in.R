fa27bb1e45b1f8bab6cea09719f1ebb7ef0e76ae
vbfelix <felix_prot@hotmail.com>
2022-12-15 20:30:43 -0300

    add tests for not_in

--- tests/testthat/test-not_in.R
+++ tests/testthat/test-not_in.R
@@ -1,3 +1,22 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("results", {
+
+  expect_equal(
+    object = not_in("a",c("a","b","c")),
+    expected = FALSE
+  )
+
+  expect_equal(
+    object = not_in("z",c("a","b","c")),
+    expected = TRUE
+  )
+
+})
+
+test_that("warning", {
+
+
+})
+
+test_that("error", {
+
 })
