b569f1050c2c65719182104debd73f7b4131c15e
vbfelix <felix_prot@hotmail.com>
2022-12-18 23:20:06 -0300

    standardization of error messages

--- R/calc_auc.r
+++ R/calc_auc.r
@@ -4,7 +4,7 @@
 #'
 #' @param x numeric vector
 #' @param y numeric vector
-#' @param limits numeric range for integration (default = x range)
+#' @param limits numeric range for integration (default = 'x' range)
 #'
 #' @return calc_auc
 #' @export
@@ -22,16 +22,16 @@
 
 calc_auc  <- function(x,y, limits = NULL) {
 
-  if(!is.numeric(x)){stop("x must be numeric.")}
+  if(!is.numeric(x)){stop("'x' must be numeric.")}
 
-  if(!is.numeric(y)){stop("y must be numeric.")}
+  if(!is.numeric(y)){stop("'y' must be numeric.")}
 
   if((is.numeric(limits) == FALSE) & (is.null(limits) == FALSE)){
-    stop("limits must be numeric or NULL.")
+    stop("'limits' must be numeric or NULL.")
   }
 
   if((length(limits) != 0) & (length(limits) != 2)){
-    stop("limits must be of length 2 or NULL.")
+    stop("'limits' must be of length 2 or NULL.")
   }
 
   tryCatch(
