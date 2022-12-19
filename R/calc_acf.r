c96741fa0c41a1d5772542e4aa46d924567977ce
vbfelix <felix_prot@hotmail.com>
2022-12-18 23:20:01 -0300

    standardization of error messages

--- R/calc_acf.r
+++ R/calc_acf.r
@@ -20,18 +20,18 @@
 
 calc_acf <- function(x,y = NULL,...){
 
-  if(!is.numeric(x)){stop("x must be numeric.")}
+  if(!is.numeric(x)){stop("'x' must be numeric.")}
 
-  if(!is.numeric(y) & !is.null(y)){stop("y must be numeric/NULL.")}
+  if(!is.numeric(y) & !is.null(y)){stop("'y' must be numeric/NULL.")}
 
-  if(!is.null(y) & (length(y) != length(x)) ){stop("x and y  must have the same length.")}
+  if(!is.null(y) & (length(y) != length(x)) ){stop("'x' and 'y'  must have the same length.")}
 
 
   if(is.null(y)){
 
     aux <- acf(x,plot = FALSE,...)
 
-    out <- tibble::tibble(
+    output <- tibble::tibble(
       acf = unlist(aux[[1]][,1,1]),
       lag = unlist(aux[[4]][,1,1])
     )
@@ -39,11 +39,11 @@ calc_acf <- function(x,y = NULL,...){
 
     aux <- ccf(x,y,plot = FALSE,...)
 
-    out <- tibble::tibble(
+    output <- tibble::tibble(
       ccf = unlist(aux[[1]][,1,1]),
       lag = unlist(aux[[4]][,1,1])
     )
   }
 
-  return(out)
+  return(output)
 }
