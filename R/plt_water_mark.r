9f71f7962d914ef44a0a49137558255ea44ebb7d
vbfelix <felix_prot@hotmail.com>
2023-01-04 18:04:12 -0300

    changed function to accept a file or path, also changed th return to already be ggplot2 compatible

--- R/plt_water_mark.r
+++ R/plt_water_mark.r
@@ -2,27 +2,40 @@
 #'
 #' @description Import png file as watermark for ggplot2
 #'
-#' @param png_file png file path
+#' @param png_file png file, already imported in R
+#' @param png_path png path to import the png file
 #'
 #' @return object to use in ggplot2
 #' @export
 #'
 #' @examples
-#'
-#'
+#'#'
 #' library(ggplot2)
 #'
 #' #A local png file is necessary
-#' logo <- plt_water_mark(path_to_local_png)
 #'
-#' ggplot()+
-#'   annotation_custom(logo)
+#' ggplot(mtcars)+
+#'   plt_water_mark(file_path = path_to_local_png)
 
 
-plt_water_mark <-function(png_file){
+plt_water_mark <-function(png_file = NULL, png_path = NULL){
 
-  png <- png::readPNG(png_file)
+  if(is.null(png_file) & is.null(png_path)){stop("You must set either 'png_file' or 'png_path'.")}
 
-  grid::rasterGrob(image = png, interpolate = FALSE)
+  if(!is.null(png_file) & !is.null(png_path)){stop("You must set either 'png_file' or 'png_path'.")}
+
+  if(is.null(png_file) & !is.null(png_path)){
+
+    png <- png::readPNG(png_file)
+
+  }
+
+  if(!is.null(png_file) & is.null(png_path)){
+
+    png <- png_file
+
+  }
+
+  ggplot2::annotation_custom(grid::rasterGrob(image = png, interpolate = FALSE))
 
 }
