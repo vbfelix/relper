0b86cd7c7d457cac67a641cdaea7f6b7b5bd20e1
vbfelix <felix_prot@hotmail.com>
2023-01-18 00:51:45 -0300

    add test for ties

--- tests/testthat/test-calc_mode.R
+++ tests/testthat/test-calc_mode.R
@@ -2,11 +2,16 @@
 test_that("Expect equal", {
 
   expect_equal(
-    object = calc_mode(c("a","a","a","b")),
+    object = calc_mode(x = c("a","a","a","b")),
     expected = "a"
   )
 
 })
 
+test_that("Ties", {
+
+  expect_warning(object = calc_mode(c("a","a","b","b")))
+
+})
 
 
