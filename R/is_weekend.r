8b84a9310d3cf4fe7224c76bfeb1dcecda55c051
vbfelix <felix_prot@hotmail.com>
2023-01-19 09:36:36 -0300

    add function is_weekend

---
+++ R/is_weekend.r
@@ -0,0 +1,28 @@
+#' Conditional check if a date is a weekend
+#'
+#' @description Return TRUE if the date vector is a weekend (saturday/sunday).
+#'
+#' @eval arg_vector("x","date")
+#'
+#' @return A boolean vector with the return from the condition check.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' is_weekend(x = Sys.time())
+#'
+#' is_weekend(x = Sys.Date())
+#'
+
+is_weekend <- function(x){
+
+  if(relper::is_datetime(x)){x <- lubridate::as_date(x)}
+
+  stop_function(arg = x,type = "date")
+
+  output <- lubridate::wday(x,label = FALSE) %in% c(1,7)
+
+  return(output)
+
+}
---
+++ tests/testthat/test-is_weekend.R
@@ -0,0 +1,35 @@
+#friday
+date1 <- lubridate::dmy("20/01/2023")
+datetime1 <- lubridate::dmy_hms("20/01/2023 10:00:00")
+
+#saturday
+date2 <- lubridate::dmy("21/01/2023")
+datetime2 <- lubridate::dmy_hms("21/01/2023 10:00:00")
+
+
+test_that("Expect TRUE", {
+
+  expect_true(is_weekend(x = date2))
+  expect_true(is_weekend(x = datetime2))
+
+})
+
+test_that("Expect FALSE", {
+
+  expect_true(is_weekend(x = date1))
+
+  expect_true(is_weekend(x = datetime1))
+
+})
+
+test_that("Wrong type", {
+
+  expect_error(is_weekend(x = 1))
+
+  expect_error(is_weekend(x = "a"))
+
+  expect_error(is_weekend(x = TRUE))
+
+
+})
+
