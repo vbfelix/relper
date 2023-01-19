f5a0b25967ce4c8e741572d576a465e0af897a55
vbfelix <felix_prot@hotmail.com>
2023-01-19 09:34:53 -0300

    add function summary_xy

---
+++ R/summary_xy.r
@@ -0,0 +1,40 @@
+#' Summary of two numeric variables
+#'
+#' @description Computation of the Pearson, Kendall and Spearman correlations coefficients.
+#'
+#' @eval arg_vector("x","numeric")
+#' @eval arg_vector("y","numeric")
+#'
+#' @return A tibble (1x3) with the computed coefficients.
+#' @export
+#'
+#' @examples
+#'
+#' x <- rnorm(100)
+#'
+#' y <- rnorm(100)
+#'
+#' plot(x,y)
+#'
+#' calc_corr(x,y)
+#'
+#'
+
+summary_xy <- function(x,y){
+
+  stop_function(arg = x,type = "numeric",length_bigger = 2)
+
+  stop_function(arg = y,type = "numeric",length_bigger = 2)
+
+  stop_two_args(arg1 = x, arg2 = y, equal_length = TRUE)
+
+  output <-
+  dplyr::tibble(
+    pearson  =  stats::cor.test(x,y,method = "pearson",na.rm = TRUE)$estimate,
+    kendall  =  stats::cor.test(x,y,method = "kendall",na.rm = TRUE)$estimate,
+    spearman =  stats::cor.test(x,y,method = "spearman",na.rm = TRUE)$estimate
+  )
+
+  return(output)
+
+}
