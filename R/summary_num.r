65449d6724710c3ff58c989d219a5d3cbcc38310
vbfelix <felix_prot@hotmail.com>
2023-01-19 13:20:30 -0300

    update doc

--- R/summary_num.r
+++ R/summary_num.r
@@ -1,17 +1,10 @@
-#' Summary  of numeric variables
+#' Summary of a numeric variable
 #'
 #' @description Summary statistics for numeric variables.
 #'
-#' @eval arg_vector("x","numeric")
-#' @eval arg_boolean("minimal",action = "return a minimal version of the summary")
+#' @details By default the summary statistics are:
 #'
-#' @return A tibble with:
 #' \cr
-#' \cr - n: the number of observations;
-#' \cr - na: the number of missing values;
-#' \cr - negative: the number of negative values;
-#' \cr - equal_zero: the number of values equal to zero;
-#' \cr - positive: the number of positive values;
 #' \cr - min: the minimum;
 #' \cr - p25: the first quartile;
 #' \cr - p50: the second quartile (median);
@@ -20,6 +13,29 @@
 #' \cr - mode: the peak density value;
 #' \cr - mean: the mean;
 #' \cr - cv: the coefficient of variation.
+#'
+#' If `type` = TRUE, the following metrics will be added:
+#'
+#' \cr
+#' \cr - n: the number of observations;
+#' \cr - na: the number of missing values;
+#' \cr - negative: the number of negative values;
+#' \cr - equal_zero: the number of values equal to zero;
+#' \cr - positive: the number of positive values.
+#'
+#' If `other_means` = TRUE, the following metrics will be added:
+#'
+#' \cr
+#' \cr - geometric_mean: the geometric mean;
+#' \cr - harmonic_mean: the harmonic mean.
+#'
+#' @eval arg_vector("x","numeric")
+#' @eval arg_boolean("type",action = "add metrics related to the variables type", default = "TRUE")
+#' @eval arg_boolean("other_means",action = "add the harmonic and geometric means")
+#'
+#' @return A tibble with the summary metrics.
+
+
 #'
 #'
 #' @export
@@ -33,47 +49,50 @@
 #' summary_num(x)
 #'
 
-summary_num <- function(x, minimal = FALSE){
+summary_num <- function(x, type = FALSE, other_means = FALSE){
 
   stop_function(arg = x,type = "numeric")
 
-  stop_function(arg = minimal,type = "logical",single_value = TRUE)
+  stop_function(arg = type,type = "logical",single_value = TRUE)
 
-  if(minimal == FALSE){
+  stop_function(arg = other_means,type = "logical",single_value = TRUE)
+
+  output <-
+    dplyr::tibble(
+      min = min(x, na.rm = TRUE),
+      p25 = stats::quantile(x,probs = .25, na.rm = TRUE),
+      p50 = stats::median(x, na.rm = TRUE),
+      p75 = stats::quantile(x,probs = .75, na.rm = TRUE),
+      max = max(x, na.rm = TRUE),
+      mode = dplyr::if_else(
+        condition = length(stats::na.omit(x)) < 3,
+        true = NA_real_ ,
+        false = relper::calc_peak_density(stats::na.omit(x))),
+      mean = mean(x, na.rm = TRUE),
+      cv = relper::calc_cv(x)
+    )
+
+
+  if(!type){
     output <-
       dplyr::tibble(
         n = length(x),
         na = sum(is.na(x)),
         negative = sum(x < 0, na.rm = TRUE),
         equal_zero = sum(x == 0, na.rm = TRUE),
-        positive = sum(x > 0, na.rm = TRUE),
-        min  = min(x, na.rm = TRUE),
-        p25  = stats::quantile(x,probs = .25, na.rm = TRUE),
-        p50  = stats::median(x, na.rm = TRUE),
-        p75  = stats::quantile(x,probs = .75, na.rm = TRUE),
-        max = max(x, na.rm = TRUE),
-        mode = dplyr::if_else(
-          length(stats::na.omit(x)) < 3,
-          NA_real_,
-          relper::calc_peak_density(stats::na.omit(x))),
-        mean = mean(x, na.rm = TRUE),
-        cv = relper::calc_cv(x)
+        positive = sum(x > 0, na.rm = TRUE)
       )
-  }else{
+    dplyr::bind_cols(output)
+  }
+
+  if(!other_means){
     output <-
-      dplyr::tibble(
-        n = length(x),
-        min  = min(x, na.rm = TRUE),
-        p25  = stats::quantile(x,probs = .25, na.rm = TRUE),
-        p50  = stats::median(x, na.rm = TRUE),
-        p75  = stats::quantile(x,probs = .75, na.rm = TRUE),
-        max = max(x, na.rm = TRUE),
-        mode = dplyr::if_else(
-          length(stats::na.omit(x)) < 3,
-          NA_real_ ,
-          relper::calc_peak_density(stats::na.omit(x))),
-        mean = mean(x, na.rm = TRUE),
-        cv = relper::calc_cv(x)
+      output %>%
+      dplyr::bind_cols(
+        dplyr::tibble(
+          geometric_mean = relper::calc_geometric_mean(x = x),
+          harmonic_mean = relper::calc_harmonic_mean(x = x)
+        )
       )
   }
 
