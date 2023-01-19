3e9151aae14eebe4efc2897574cafdb96092c7bd
vbfelix <felix_prot@hotmail.com>
2023-01-19 13:21:03 -0300

    update test for new args

--- tests/testthat/test-summary_num.R
+++ tests/testthat/test-summary_num.R
@@ -1,42 +1,16 @@
-summary_num_full <-
-  structure(list(n = 2L, na = 0L, negative = 0L, equal_zero = 0L,
-                 positive = 2L, min = 1L, p25 = c(`25%` = 1.25), p50 = 1.5,
-                 p75 = c(`75%` = 1.75), max = 2L, mode = NA_real_, mean = 1.5,
-                 cv = 0.47), row.names = c(NA, -1L), class = c("tbl_df", "tbl",
-                                                               "data.frame"))
-summary_num_minimal <-
-  structure(list(n = 2L, min = 1L, p25 = c(`25%` = 1.25), p50 = 1.5,
-                 p75 = c(`75%` = 1.75), max = 2L, mode = NA_real_, mean = 1.5,
-                 cv = 0.47), row.names = c(NA, -1L), class = c("tbl_df", "tbl",
-                                                               "data.frame"))
-
-
-test_that("Expected equal", {
-
-  expect_equal(
-    object = summary_num(x = 1:2),
-    expected =  summary_num_full
-  )
-
-  expect_equal(
-    object = summary_num(x = 1:2,minimal = TRUE),
-    expected =  summary_num_minimal
-  )
-
-})
-
-
-
 test_that("Wrong type", {
 
   expect_error(object = summary_num(x = "a"))
 
   expect_error(object = summary_num(x = TRUE))
 
-  expect_error(object = summary_num(x = 1:2,minimal = 2))
+  expect_error(object = summary_num(x = 1:2,type = 2))
 
-  expect_error(object = summary_num(x = 1:2,minimal = "a"))
+  expect_error(object = summary_num(x = 1:2,type = "a"))
 
+  expect_error(object = summary_num(x = 1:2,other_means = 2))
+
+  expect_error(object = summary_num(x = 1:2,other_means = "a"))
 
 })
 
