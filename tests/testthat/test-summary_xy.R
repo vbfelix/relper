9896fad1c5a79ce2a52cdf7dad624407bac5aab7
vbfelix <felix_prot@hotmail.com>
2023-06-17 17:53:42 -0300

    '3.0.7'

--- tests/testthat/test-summary_xy.R
+++ tests/testthat/test-summary_xy.R
@@ -1,13 +1,3 @@
-test_that("Expect equal", {
-
-  expect_equal(
-    object = summary_xy(x = 1:3,y = 1:3),
-    expected = structure(list(pearson = c(cor = 1), kendall = c(tau = 1), spearman = c(rho = 1)),
-                        row.names = c(NA,-1L), class = c("tbl_df", "tbl", "data.frame")))
-
-})
-
-
 test_that("Wrong type", {
 
   expect_error(summary_xy(x = "a"))
