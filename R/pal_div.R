569efb5eef69e0eb14c24b581b0de0658a2c69d4
vbfelix <felix_prot@hotmail.com>
2024-01-01 09:23:23 -0300

    Increment version number to 3.10.0

--- R/pal_div.R
+++ R/pal_div.R
@@ -1,8 +1,71 @@
+#' List of palettes
+#' @export
+
+pals_div <- list(
+
+  arcane =
+    c("#7D5449", "#967D6E", "#BEA698", "#3A5C6C", "#2B3548"),
+
+  auto_compadecida =
+    c("#5D5D4B","#8E8A76","#C2B09F","#E0A71C","#C76E4A","#A13D20")
+
+  casa_de_papel =
+    c("#6B362B", "#B83C36", "#B38262", "#D4C1A0", "#94A994", "#546A57","#35453B"),
+
+  deadly_class =
+    c("#552643", "#A03663", "#AD849C", "#E1B7C2", "#5C5A8A", "#16275D"),
+
+  dexter =
+    c("#046382","#12A6C5","#6CC9DE","#EAE0CF","#C69369","#7D3728"),
+
+  godzilla_vs_kong =
+    c("#1F191A","#25403F","#2D7A73","#44B4AC","#A0CBBA","#D9996E","#E45A2E","#BB2F10","#6F2014"),
+
+  good_place =
+    c("#1C262B","#275D86","#237EB9","#8BA8C1","#E4E8E0","#F1DAA1","#F8D26E","#B08D3F","#775B24"),
+
+  homeland =
+    c("#31302D","#655B4D","#8C8274","#B9A791","#D2C5BE","#91B3C7","#7096AC","#4E7890","#3D5460"),
+
+  laura_dean =
+    rev(c("#483A3A","#8A6253","#AB8B78","#DBA08B","#F8E8E5","#F5C9CD","#F1A4B1","#DB5E69")),
+
+  racionais =
+    c("#3E2C2A", "#75472E", "#9A8365", "#DBC897", "#85BFE4", "#5298D5","#366FC1"),
+
+  sandman =
+    c("#876453", "#B7906B", "#E1C688", "#A7B79E", "#70ACA0"),
+
+  scott_pilgrim =
+    c("#86141E","#A5262D","#E0413B","#CBA298","#A17267","#5B2E23"),
+
+  the_americans =
+    c("#871307", "#D00F05", "#EA8E0F", "#F8CE1D", "#09A8DF", "#2B88AA","#0F2B77"),
+
+  the_expanse =
+    c("#521C15", "#A93624", "#B36D4C", "#D6A281", "#E6EBE4", "#9DB7BA","#61777E", "#3E4B4F"),
+
+  the_marvels =
+    c("#0C0390","#3F069A","#730AA4","#B43252","#F65B01","#B83601","#7B1102"),
+
+  vox_machina =
+    c("#493953", "#746776", "#B1898B", "#CDBAB2", "#B5CCDE", "#9EADBA","#7991A2"),
+
+  wanda_vision =
+    c("#501715", "#8F2A1F", "#C14E3E", "#E59177", "#DFCBCC", "#956E83","#266288", "#213E5E", "#181F38"),
+
+  white_lotus =
+    c("#486558","#7C836D","#A2A683","#E4DBB1","#F6F2C9","#D1B98F","#C57B69","#934241")
+
+)
+
+#'
 #' Palettes for divergent scale
 #'
 #' @description Palette for a divergent scale, with the following palettes:
 #'  \cr
 #'  \cr - arcane: 5 colors, inspired by the animation Arcane.
+#'  \cr - auto_compadecida: 6 colors, inspired by the movie O Auto da Compadecida.
 #'  \cr - casa_de_papel: 7 colors, inspired by the TV series La Casa de Papel.
 #'  \cr - deadly_class: 6 colors, inspired by the TV series Deadly Class.
 #'  \cr - dexter: 6 colors, inspired by the TV series Dexter.
@@ -36,78 +99,17 @@
 
 pal_div <-
   function(
-    name = c("arcane","casa_de_papel","deadly_class","dexter","godzilla_vs_kong","good_place",
-             "homeland",
-             "laura_dean","racionais","sandman","scott_pilgrim",
-             "the_americans","the_expanse","the_marvels","vox_machina","wanda_vision","white_lotus"),
+    name = "arcane",
     reverse = FALSE
     ){
 
     stopifnot(is.character(name), length(name) == 1)
 
-    name <- match.arg(name)
-
     stopifnot(is.logical(reverse), length(reverse) == 1)
 
-    arcane <-
-      c("#7D5449", "#967D6E", "#BEA698", "#3A5C6C", "#2B3548")
+    output <- pals_div[[name]]
 
-    casa_de_papel <-
-      c("#6B362B", "#B83C36", "#B38262", "#D4C1A0", "#94A994", "#546A57","#35453B")
-
-    deadly_class <-
-      c("#552643", "#A03663", "#AD849C", "#E1B7C2", "#5C5A8A", "#16275D")
-
-    dexter <-
-      c("#046382","#12A6C5","#6CC9DE","#EAE0CF","#C69369","#7D3728")
-
-    godzilla_vs_kong <-
-      c("#1F191A","#25403F","#2D7A73","#44B4AC","#A0CBBA","#D9996E","#E45A2E","#BB2F10","#6F2014")
-
-    good_place <-
-      c("#1C262B","#275D86","#237EB9","#8BA8C1","#E4E8E0","#F1DAA1","#F8D26E","#B08D3F","#775B24")
-
-    homeland <-
-      c("#31302D","#655B4D","#8C8274","#B9A791","#D2C5BE","#91B3C7","#7096AC","#4E7890","#3D5460")
-
-    laura_dean <-
-      rev(c("#483A3A","#8A6253","#AB8B78","#DBA08B","#F8E8E5","#F5C9CD","#F1A4B1","#DB5E69"))
-
-    racionais <-
-      c("#3E2C2A", "#75472E", "#9A8365", "#DBC897", "#85BFE4", "#5298D5",
-        "#366FC1")
-
-    sandman <-
-      c("#876453", "#B7906B", "#E1C688", "#A7B79E", "#70ACA0")
-
-    scott_pilgrim <-
-      c("#86141E","#A5262D","#E0413B","#CBA298","#A17267","#5B2E23")
-
-    the_americans <-
-      c("#871307", "#D00F05", "#EA8E0F", "#F8CE1D", "#09A8DF", "#2B88AA",
-        "#0F2B77")
-
-    the_expanse <-
-      c("#521C15", "#A93624", "#B36D4C", "#D6A281", "#E6EBE4", "#9DB7BA",
-        "#61777E", "#3E4B4F")
-
-    the_marvels_ramp <-
-      colorRampPalette(c("#0c0390","#730aa4","#f65b01","#7b1102"))
-
-    the_marvels <- the_marvels_ramp(7)
-
-    vox_machina <-
-      c("#493953", "#746776", "#B1898B", "#CDBAB2", "#B5CCDE", "#9EADBA",
-                     "#7991A2")
-
-    wanda_vision <-
-      c("#501715", "#8F2A1F", "#C14E3E", "#E59177", "#DFCBCC", "#956E83",
-        "#266288", "#213E5E", "#181F38")
-
-    white_lotus <-
-      c("#486558","#7C836D","#A2A683","#E4DBB1","#F6F2C9","#D1B98F","#C57B69","#934241")
-
-    output <- eval(parse(text = match.arg(name)))
+    stopifnot(is.character(output))
 
     if(reverse){output <- rev(output)}
 
@@ -115,6 +117,5 @@ pal_div <-
 
   }
 
-# pal_vignette(pal_div)
 
 
--- R/pal_qua.R
+++ R/pal_qua.R
@@ -1,7 +1,89 @@
+#' List of palettes
+#' @export
+
+pals_qua <- list(
+
+  berlim = c("#0A6D68","#BF2E23","#735738","#CDAE07","#161C15"),
+
+  blue_eye_samurai = c("#214442","#0C696D","#45ABAF","#EBAE5B","#E7422A","#7D2D1E"),
+
+  bojack_horseman =
+    c("#EB636B","#9E674C", "#FAC877", "#9464A2","#3C2533"),
+
+  cowboy_bebop =
+    c("#1B5260", "#CA301D", "#B7B5A4", "#612023","#219EBF"),
+
+  doctor_who =
+    c("#DA4327", "#15406B", "#F8CE6F", "#6F3C37","#C69ABE", "#F79336"),
+
+  elemental =
+    c("#f4230c", "#35b2fc", "#286e2b","#886fd9","#ec6a15","#56f1c6"),
+
+  final_space =
+    c("#9FFA6F", "#AC1132", "#EDDD30", "#F5D9D2"),
+
+  gurren_lagann =
+    c("#9F2B2E", "#424F91", "#F27137", "#DA70C1", "#89646A"),
+
+  heartstopper =
+    c("#41A99B","#F7CC99","#103336","#C79274","#A2E0E6"),
+
+  kaleidoscope =
+    c("#92313c","#247ebd","#db904f","#5eb05b","#9063c0","#f6ec79","#1a2545"),
+
+  kick_ass =
+    c("#F3DD42","#654B6D","#1F8163","#763B16","#A1A289","#040303"),
+
+  killers_flower_moon =
+    c("#B0456E","#AE8E6C","#53417A","#DFC8CF","#141633"),
+
+  legion =
+    c("#E6318A", "#DAC856","#15729E", "#F8A525", "#625E3C", "#992A89","#F7743D", "#A87040", "#A52830"),
+
+  mario_bros =
+    c("#f11e15","#0dd105","#fd4ab5","#0c8cfa","#ffde15","#8310bf","#a3431b"),
+
+  moonlight =
+    rev(c("#6B4A83","#17647D","#332E5A","#04BEB8","#8BB0DA")),
+
+  ms_marvel =
+    c("#4D2B5A","#52B054","#E3DE38","#DB3D52","#E2BF95","#270D23"),
+
+  omitb =
+    c("#A98A1B", "#385A4B", "#911304", "#CDB283","#26211C"),
+
+  pulp_fiction =
+    c("#483F5C", "#D5272B", "#847298", "#DD6652", "#E1CAD9", "#F7CF62"),
+
+  red_dead =
+    c("#C80710","#080202","#FCA208","#78645C","#FCA208","#E0AC8B"),
+
+  rick_and_morty =
+    c("#1A1D3C", "#488A4F", "#F5C7AB", "#D2D964", "#EB8827", "#249FB5","#945B43", "#8DBF4D"),
+
+  ted_lasso =
+    c("#094895","#6EA540","#AF453B","#C4AF9C","#596172"),
+
+  this_is_us =
+    c("#2E2C33", "#E1AE1D", "#614330", "#968A82", "#E4B999"),
+
+  versace =
+    c("#092391","#E87539","#982B52","#FCCE30","#763AA0","#520E34","#F8BD72","#DD518F"),
+
+  ww84 =
+    c("#942415","#15D374","#241210","#E8D025","#BE737B","#86EDC9"),
+
+  young_sheldon =
+    c("#C51C1E","#3D6B5C","#CB9041","#51828B","#8C4D32")
+
+)
+
 #' Palettes for qualitative scale
 #'
 #' @description Palettes for a qualitative scale, with the following palettes:
 #'  \cr
+#'  \cr - berlim: 5 colors, inspired by the TV series Berlim.
+#'  \cr - blue_eye_samurai: 6 colors, inpired by the TV series Blue Eye Samurai.
 #'  \cr - bojack_horseman: 5 colors, inspired by the animation BoJack Horseman.
 #'  \cr - cowboy_bebop: 5 colors, inspired by the animation Cowboy Bebop.
 #'  \cr - doctor_who: 6 colors, inspired by the TV series Doctor Who.
@@ -11,6 +93,7 @@
 #'  \cr - heartstopper: 5 colors, inspired by the TV series Heartstopper.
 #'  \cr - kaleidoscope: 7 colors, inspired by the TV series Kaleidoscope.
 #'  \cr - kick_ass: 6 colors, inspired by the movie Kick-ass.
+#'  \cr - killers_flower_moon: 5 colors, inspired byt the movie Killers of the Flower Moon.
 #'  \cr - legion: 9 colors, inspired by the TV series Legion.
 #'  \cr - mario_bros: 7 colors, inspired by the movie The Super Mario Brothers.
 #'  \cr - moonlight: 5 colors, inspired by the movie Moonlight.
@@ -40,89 +123,17 @@
 
 pal_qua <-
   function(
-    name = c("bojack_horseman","cowboy_bebop",
-             "doctor_who","elemental","final_space","gurren_lagann","heartstopper","kaleidoscope","kick_ass",
-             "legion","omitb","mario_bros","moonlight","ms_marvel","pulp_fiction","red_dead",
-             "rick_and_morty","ted_lasso",
-             "this_is_us","versace","ww84","young_sheldon"),
+    name = "young_sheldon",
     reverse = FALSE
     ){
 
     stopifnot(is.character(name), length(name) == 1)
 
-    name <- match.arg(name)
-
     stopifnot(is.logical(reverse), length(reverse) == 1)
 
-    bojack_horseman <-
-      c("#EB636B","#9E674C", "#FAC877", "#9464A2","#3C2533")
+    output <- pals_qua[[name]]
 
-    cowboy_bebop <-
-      c("#1B5260", "#CA301D", "#B7B5A4", "#612023","#219EBF")
-
-    doctor_who <-
-      c("#DA4327", "#15406B", "#F8CE6F", "#6F3C37","#C69ABE", "#F79336")
-
-    elemental <-
-      c("#f4230c", "#35b2fc", "#286e2b","#886fd9","#ec6a15","#56f1c6")
-
-    final_space <-
-      c("#9FFA6F", "#AC1132", "#EDDD30", "#F5D9D2")
-
-    gurren_lagann <-
-      c("#9F2B2E", "#424F91", "#F27137", "#DA70C1", "#89646A")
-
-    heartstopper <-
-      c("#41A99B","#F7CC99","#103336","#C79274","#A2E0E6")
-
-    kaleidoscope <-
-      c("#92313c","#247ebd","#db904f","#5eb05b","#9063c0","#f6ec79","#1a2545")
-
-    kick_ass <-
-      c("#F3DD42","#654B6D","#1F8163","#763B16","#A1A289","#040303")
-
-    legion <-
-      c("#E6318A", "#DAC856","#15729E", "#F8A525", "#625E3C", "#992A89",
-        "#F7743D", "#A87040", "#A52830")
-
-    mario_bros <-
-      c("#f11e15","#0dd105","#fd4ab5","#0c8cfa","#ffde15","#8310bf","#a3431b")
-
-    moonlight <-
-      rev(c("#6B4A83","#17647D","#332E5A","#04BEB8","#8BB0DA"))
-
-    ms_marvel <-
-      c("#4D2B5A","#52B054","#E3DE38","#DB3D52","#E2BF95","#270D23")
-
-    omitb <-
-      c("#A98A1B", "#385A4B", "#911304", "#CDB283","#26211C")
-
-    pulp_fiction <-
-      c("#483F5C", "#D5272B", "#847298", "#DD6652", "#E1CAD9", "#F7CF62")
-
-    red_dead <-
-      c("#C80710","#080202","#FCA208","#78645C","#FCA208","#E0AC8B")
-
-    rick_and_morty <-
-      c("#1A1D3C", "#488A4F", "#F5C7AB", "#D2D964", "#EB8827", "#249FB5",
-        "#945B43", "#8DBF4D")
-
-    ted_lasso <-
-      c("#094895","#6EA540","#AF453B","#C4AF9C","#596172")
-
-    this_is_us <-
-      c("#2E2C33", "#E1AE1D", "#614330", "#968A82", "#E4B999")
-
-    versace <-
-      c("#092391","#E87539","#982B52","#FCCE30","#763AA0","#520E34","#F8BD72","#DD518F")
-
-    ww84 <-
-      c("#942415","#15D374","#241210","#E8D025","#BE737B","#86EDC9")
-
-    young_sheldon <-
-      c("#C51C1E","#3D6B5C","#CB9041","#51828B","#8C4D32")
-
-    output <- eval(parse(text = match.arg(name)))
+    stopifnot(is.character(output))
 
     if(reverse){output <- rev(output)}
 
@@ -130,5 +141,4 @@ pal_qua <-
 
   }
 
-# pal_vignette(pal_qua)
 
