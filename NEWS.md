d14bd1dc1c8f61b1bcd29a88f5d5484856c606b1
vbfelix <felix_prot@hotmail.com>
2025-01-20 22:05:50 -0300

    3.14.3 plt_pinpoint upgrade

--- NEWS.md
+++ NEWS.md
@@ -4,30 +4,31 @@
 >
 > *These additions transform the package into a comprehensive "Data Insight Toolbox," empowering users to gain deeper insights and perform advanced analyses on their datasets. Additionally, some functions have been renamed for better clarity and consistency.*
 
-## relper 3.14.2
+## relper 3.14.3
 
 **Additions**
 
--   count2
+-   Function count2
 
 ## relper 3.13.1
 
 **Additions**
 
--   replace_boolean, replace_na
+-   Functions replace_boolean, replace_na
+-   Parameters label and add_line to plt_pinpoint
 
 
 ## relper 3.12.0
 
 **Additions**
 
--   count_na
+-   Function count_na
 
 ## relper 3.11.3
 
 **Additions**
 
--   calc_perc
+-   Function calc_perc
 
 **Improvements**
 
--- R/plt_pinpoint.R
+++ R/plt_pinpoint.R
@@ -4,10 +4,13 @@
 #'
 #' @eval arg_value("x","numeric")
 #' @eval arg_value("y","numeric")
-#' @eval arg_value("size","numeric",default = "3")
-#' @eval arg_value("color","character",default = "'firebrick3'", action = "will be the line color")
+#' @eval arg_value("size","numeric",default = "3", action = "will be the point size")
+#' @eval arg_value("color","character",default = "'firebrick3'", action = "will be the point color")
+#' @eval arg_boolean("add_line",default = "TRUE", action = "add lines that intercept the point")
 #' @eval arg_value("linetype","character",default = "'dashed'", action = "will be the line type")
 #' @eval arg_value("linewidth","numeric",default = "0.75", action = "will be the line width")
+#' @eval arg_value("label","character",default = "NULL", action = "will add a label on top of the point")
+
 #'
 #' @return A ggplot2 object.
 #'
@@ -26,7 +29,7 @@
 #' plot + plt_pinpoint(x = 18,y = 25)
 #'
 
-plt_pinpoint <- function(x,y,color = "firebrick3",size = 3, linetype = "dashed",linewidth = .75){
+plt_pinpoint <- function(x,y,color = "firebrick3",size = 3, add_line = TRUE, linetype = "dashed",linewidth = .75,label = NULL){
 
   stopifnot(is.numeric(x))
 
@@ -42,19 +45,45 @@ plt_pinpoint <- function(x,y,color = "firebrick3",size = 3, linetype = "dashed",
 
   stopifnot(is.numeric(linewidth), length(linewidth) == 1, linewidth > 0)
 
-  list(
-    ggplot2::geom_vline(
-      xintercept = x,color = color,linetype = linetype, linewidth = linewidth
-    ),
-    ggplot2::geom_hline(
-      yintercept = y,color = color,linetype = linetype, linewidth = linewidth
-    ),
-    ggplot2::annotate(
-      geom = "point",
-      x = x,
-      y = y,
-      color = color,
-      size = size
+  output <-
+    list(
+      ggplot2::annotate(
+        geom = "point",
+        x = x,
+        y = y,
+        color = color,
+        size = size
+      )
     )
-  )
+
+  if(add_line){
+
+    output <-
+      list(
+        output,
+        ggplot2::geom_vline(
+          xintercept = x,color = color,linetype = linetype, linewidth = linewidth
+        ),
+        ggplot2::geom_hline(
+          yintercept = y,color = color,linetype = linetype, linewidth = linewidth
+        )
+      )
+  }
+
+  if(!is.null(label)){
+    output <-
+      list(
+        output,
+        ggplot2::annotate(
+          geom = "text",
+          x = x,
+          y = 1.05*y,
+          label = label,
+          color = color
+        )
+      )
+  }
+
+  return(output)
+
 }
